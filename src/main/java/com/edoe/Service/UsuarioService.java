f13cf03e4fe0338042757fc53d82a41d3827134f
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2019-08-23 12:53:30 -0300

    mais uma

--- src/main/java/com/edoe/Service/UsuarioService.java
+++ src/main/java/com/edoe/Service/UsuarioService.java
@@ -70,13 +70,13 @@ public class UsuarioService {
 		if (usuarioToUpdate == null)
 			throw new Exception("Usuario nao encontrado: " + id);
 		
-		if (nome != null || !nome.equals(""))
+		if (nome != null && !nome.equals(""))
 			usuarioToUpdate.setNome(nome);
 		
-		if (email != null || !email.equals(""))
+		if (email != null && email.equals(""))
 			usuarioToUpdate.setEmail(email);
 		
-		if (celular != null|| !celular.equals(""))
+		if (celular != null && !celular.equals(""))
 			usuarioToUpdate.setCelular(celular);
 		
 		usuarioDAO.save(usuarioToUpdate);
