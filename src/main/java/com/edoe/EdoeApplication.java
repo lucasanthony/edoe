c5652e75ef65dd63076c85b3835ce63fa76b0c4d
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2019-08-23 12:37:30 -0300

    remover a atualizar

--- src/main/java/com/edoe/EdoeApplication.java
+++ src/main/java/com/edoe/EdoeApplication.java
@@ -3,7 +3,7 @@ package com.edoe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import easyaccept.EasyAccept;
+//import easyaccept.EasyAccept;
 
 
 @SpringBootApplication
@@ -11,8 +11,8 @@ public class EdoeApplication {
 
 	public static void main(String[] args) {
 		// DESCOMENTA AQUI PRA RODAR OS TESTES DE ACEITAÇÃO
-		args = new String[] { "com.edoe.Service.UsuarioService", "Acceptance_test/use_case_1.txt"};
-		EasyAccept.main(args);
+		//args = new String[] { "com.edoe.Service.UsuarioService", "Acceptance_test/use_case_1.txt"};
+		//EasyAccept.main(args);
 
 		SpringApplication.run(EdoeApplication.class, args);
 	}
