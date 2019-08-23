d0fd1ced85181025314bdbe13d1bb4922e779b2d
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2019-08-23 07:45:04 -0300

    EasyAccept conectado

--- src/main/java/com/edoe/EdoeApplication.java
+++ src/main/java/com/edoe/EdoeApplication.java
@@ -3,10 +3,16 @@ package com.edoe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import easyaccept.EasyAccept;
+
 @SpringBootApplication
 public class EdoeApplication {
 
 	public static void main(String[] args) {
+		// DESCOMENTA AQUI PRA RODAR OS TESTES DE ACEITAÇÃO
+//		args = new String[] { "com.edoe.Controllers.UsuarioController", "Acceptance_test/use_case_1.txt"};
+//		EasyAccept.main(args);
+
 		SpringApplication.run(EdoeApplication.class, args);
 	}
 
