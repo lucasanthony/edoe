4636e5cee8ac2996aef8706e97b942c236637f43
Pedro Farias <pedro.wanderley@ccc.ufcg.edu.br>
2019-09-05 17:45:38 -0300

    adicionando arquivos de config

--- src/main/java/com/edoe/EdoeApplication.java
+++ src/main/java/com/edoe/EdoeApplication.java
@@ -1,9 +1,13 @@
 package com.edoe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class EdoeApplication {
 
 	public static void main(String[] args) {
